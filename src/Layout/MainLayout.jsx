c91211af4a5f6e07e715227e56d223a9a606abac
ariaan ahmed <you@example.com>
2023-06-27 23:06:55 +0600

    Spinner and page responsive fixed

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,15 +1,42 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
 import Navbar from '../pages/Shared/Navbar/Navbar';
+import { ClipLoader } from 'react-spinners';
+import { useEffect, useState } from 'react';
 
 const MainLayout = () => {
-    return (
-        <div style={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
-            <Navbar/>
-            <Outlet/>
-            <Footer/>
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [loading]);
+
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      {loading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <ClipLoader size={30} color={'#F37A24'} loading={loading} />
         </div>
-    );
+      ) : (
+        <>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Lottie from "lottie-react";
 import coding from '../../../assets/coding.json';
+import Typical from "react-typical";
 
 const Banner = () => {
 
@@ -11,19 +12,36 @@ const Banner = () => {
     }, [])
     return (
         <div>
-            <div data-aos="fade-up" data-aos-anchor-placement="center-center" >
-                <div className="py-2 pl-10">
+            <div /* data-aos="fade-up" data-aos-anchor-placement="center-center"  */>
+                <div>
                     <div className="flex flex-col md:flex-row-reverse w-full items-center justify-around space-y-8">
                         <div>
-                            <Lottie animationData={coding} loop={true}  className="ml-auto w-full" style={{width: '32rem'}}></Lottie>
+                            <Lottie animationData={coding} loop={true}  className="ml-auto w-full" /* style={{width: '31rem'}} */ ></Lottie>
                         </div>
-                        <div className="w-full space-y-3">
+                        <div className="space-y-3">
                             <h1 className="text-center font-bold text-">
-                                <span className="md:text-3xl text-2xl">Ariaan Ahmed</span>
+                                <span className="md:text-3xl text-2xl">Hi, Im Ariaan Ahmed</span>
                                 <br />
                             </h1>
-                            <div className="text-center font-bold md:text-5xl text-3xl">MERN Stack Developer</div>
-
+                            <h2 className="text-center font-bold md:text-5xl text-3xl">MERN Stack Developer</h2>
+                            <p className="text-center">I am curious about <Typical
+                                    className='capitalize'
+                                    loop={Infinity}
+                                    wrapper="b"
+                                    steps={[
+                                        'html', 1000,
+                                        'tailwind', 1000,
+                                        'javascript', 1000,
+                                        'bootstrap', 1000,
+                                        'react', 1000,
+                                        'expressjs', 1000,
+                                        'firebase', 1000,
+                                        'mongodb', 1000,
+                                        'nodejs', 1000,
+                                        'jwt', 1000,
+                                    ]}
+                                >React</Typical>
+                            </p>
                             <div className="text-center space-x-2 flex-col items-center md:flex-row space-y-4">
                                 <Link className="btn btn-warning text-center"> Project </Link>
                                 <Link className="btn btn-light btn-outline text-center"> Resume </Link>
